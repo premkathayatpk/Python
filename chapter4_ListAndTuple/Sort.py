7c5f08f9b31c35674785f8fde456dc2aef7e2259
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-08-15 18:41:34 +0545

    list

---
+++ chapter4_ListAndTuple/Sort.py
@@ -0,0 +1,12 @@
+
+marks=[]
+f1=input("Enter marks name: ")
+marks.append(f1)
+f2=input("Enter marks name: ")
+marks.append(f2)
+f3=input("Enter marks name: ")
+marks.append(f3)
+f4=input("Enter marks name: ")
+marks.append(f4)
+print(marks)
+marks.sort
