f32878c144ce6215a8ff30ff03a8fe828adb8697
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-08-20 20:39:29 +0545

    update

---
+++ chapter5_DictionaryANDSet/DicKey_Value.py
@@ -0,0 +1,13 @@
+e={}
+l=input("Enter your fav. language: ")
+e.update({"Ram":f"{l}"})
+l=input("Enter your fav. language: ")
+e.update({"Sita":f"{l}"})
+l=input("Enter your fav. language: ")
+e.update({"Hari":f"{l}"})
+l=input("Enter your fav. language: ")
+e.update({"Gita":f"{l}"})
+
+#e.update({"Ram":f"{l}"}) --it replace the ram language
+
+print(e)
