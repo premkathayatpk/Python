c01d0341242d995bcedfdc06495ae77ca893b8f6
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-08-21 19:46:37 +0545

    mul table

---
+++ chapter7_loop/MultiplicationTable.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+"""
+# using for loop
+for i in range(1,11):
+    print(f"{num} X {i} = {num*i}\n")
+"""
+
+#using while loop
+j=1
+while j < 11:
+    print(f"{num} X {j} = {num*j}\n")
+    j+=1
