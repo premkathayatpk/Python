1b8823941e2023a958be54ffa9cbf79e66f11667
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-08-26 21:41:10 +0545

    first class

---
+++ chapter10_OOP/firstClass.py
@@ -0,0 +1,15 @@
+
+class programmer:
+    company="Microsoft"
+    def __init__(self,name,salary,address):
+        self.name=name
+        self.salary=salary
+        self.address=address
+
+
+
+p1=programmer("Prem",30000,"Ktm")
+print(p1.name,p1.salary,p1.address)
+
+p2=programmer("Ram",20000,"Bkt")
+print(p2.name,p2.salary,p2.address)
